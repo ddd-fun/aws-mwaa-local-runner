259c9a0b735d68a5b6a1c4e5221debaf06a188ad
Volodymyr Lazoryshyn <volodymyr.lazoryshyn@autoscout24.com>
2022-11-30 20:50:11 +0100

    added comments

--- dags/emr_python_abstraction.py
+++ dags/emr_python_abstraction.py
@@ -18,10 +18,13 @@ S3_ARTIFACT = "s3://as24data-artifacts/mesh-team/pipeline/"
 S3_DATA_LOCATION = "s3://as24data-processed/mesh-team/pipeline/"
 
 with DAG("emr_python_abstraction", default_args=default_args, schedule_interval=timedelta(1), tags=['workshop']) as dag:
+    # infrastructure workflow is behind EmrCluster abstraction!
     with EmrCluster(dag) as emr:
+        # here only app flow is defined
         emr.add_spark_steps("spark_transform", SparkUtils.spark_app(S3_ARTIFACT, "com.as24.meshteam.SparkApp"))
         emr.add_spark_steps("spark_publish_result", SparkUtils.publish_hive_table(S3_DATA_LOCATION, "mesh_team", "table_1"))
 
+    # we could extend workflow with extra steps
     op2 = BashOperator(task_id=f"quality_checks", bash_command=f"echo 'team_quality_cheks'")
 
     emr.get_emr_flow() >> op2
